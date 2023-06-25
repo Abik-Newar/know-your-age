aad567556a9e3f5c6ce5cf95bf771773edbb4b8e
Abik Newar <abiknewar2002@gmail.com>
2023-06-25 22:07:53 +0530

    complete calculation part and adjustments

--- js/index.js
+++ js/index.js
@@ -12,9 +12,10 @@ btn.addEventListener('click', validation); // Work on event Listening (click)
 
 function validation(){ //function which run the whole validation code
 
-    monthValid();
     dayValid();
+    monthValid();
     yearValid();
+
 
 }
 
@@ -29,18 +30,21 @@ function dayValid(){
         invalid.style.display = "block";
         empty.style.display = "none";
         document.getElementById("day").style.border = "1px solid red";
+        document.getElementById("dayAge").innerHTML = "--";
     }
 
     else if (day.length < 1) {
         empty.style.display = "block";
         invalid.style.display = "none";
         document.getElementById("day").style.border = "1px solid red";
+        document.getElementById("dayAge").innerHTML = "--";
     }
 
     else{
         invalid.style.display = "none";
         empty.style.display = "none";
         document.getElementById("day").style.border = "1px solid black";
+        dayCal();
     }
 }
 
@@ -56,18 +60,21 @@ function monthValid(){
         invalidmonth.style.display = "block";
         emptymonth.style.display = "none";
         document.getElementById("month").style.border = "1px solid red";
+        document.getElementById("monthAge").innerHTML = "--";
     }
 
     else if (month.length < 1) {
         emptymonth.style.display = "block";
         invalidmonth.style.display = "none";
         document.getElementById("month").style.border = "1px solid red";
+        document.getElementById("monthAge").innerHTML = "--";
     }
 
     else {
         invalidmonth.style.display = "none";
         emptymonth.style.display = "none";
         document.getElementById("month").style.border = "1px solid black";
+        monthCal();
     }
 }
 
@@ -99,6 +106,7 @@ function yearValid(){
         invalidyear.style.display = "none";
         emptyyear.style.display = "none";
         document.getElementById("year").style.border = "1px solid black";
+        yearCal();
     }
 }
 
@@ -109,7 +117,7 @@ function yearValid(){
 
 const date = new Date();
 
-const dayCurrent = date.getDate();
+let dayCurrent = date.getDate();
 
 const monthCurrent = date.getMonth() + 1;
 
@@ -118,5 +126,75 @@ const yearCurrent = date.getFullYear();
 // working on the mathematical solution or so called Algorithms
 // working on the mathematical solution or so called Algorithms
 // working on the mathematical solution or so called Algorithms
+
+
+
+
+function dayCal(){
+    let dayDob = document.getElementById("day").value;
+    
+    if (dayDob > dayCurrent) {
+        
+        let dayAdded = dayCurrent + 31;
+        let dayAge = dayAdded - dayDob;
+
+        document.getElementById("dayAge").innerHTML = dayAge;
+        
+    }
+    else{
+        let dayAge = dayCurrent - dayDob;
+        document.getElementById("dayAge").innerHTML = dayAge;
+    }
+}
+
+
+
+function monthCal(){
+    let monthDob = document.getElementById("month").value;
+    let dayDob = document.getElementById("day").value;
+    if (monthDob > monthCurrent) {
+        
+        let monthAdded = monthCurrent + 12;
+        if (dayDob > dayCurrent) {
+            var monthSub = monthAdded - 1;
+            var monthAge = monthSub - monthDob;
+            document.getElementById("monthAge").innerHTML = monthAge;
+        }
+        else{
+            var monthAge = monthAdded - monthDob;
+            document.getElementById("monthAge").innerHTML = monthAge;
+        }
+    }
+    else if (dayDob > dayCurrent) {
+        var monthSub = monthCurrent - 1;
+        if (monthDob > monthCurrent) {
+            let monthAdded = monthSub + 12;
+            let monthAge = monthAdded - monthDob;
+            document.getElementById("monthAge").innerHTML = monthAge;
+        }
+        else{
+            var monthAge = monthSub - monthDob;
+            document.getElementById("monthAge").innerHTML = monthAge;
+        }
+    }
+    else{
+        let monthAge = monthCurrent - monthDob;
+        document.getElementById("monthAge").innerHTML = monthAge;
+    }
+}
+
+function yearCal(){
+    const monthDob = document.getElementById("month").value;
+    const monthSub = monthCurrent - 1;
+    const yearDob = document.getElementById("year").value;
+    if (monthCurrent < monthDob || monthSub < monthDob) {
+        let yearSub = yearCurrent - 1;
+        let yearAge = yearSub - yearDob
+        document.getElementById("yearAge").innerHTML = yearAge;
+    } else {
+        let yearAge = yearCurrent - yearDob
+        document.getElementById("yearAge").innerHTML = yearAge;
+    }
+}
 
 
